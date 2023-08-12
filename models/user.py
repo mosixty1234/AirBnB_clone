ac030db585b9915e6d59b34931af9c358ffe68ae
mosixty1234 <mosixtysipho11@gmail.com>
2023-08-12 16:05:02 -0700

    task 8

--- models/user.py
+++ models/user.py
@@ -1,20 +1,15 @@
 #!/usr/bin/python3
-"""
-a class User that inherits from BaseModel
-"""
+
 from models.base_model import BaseModel
 
-
 class User(BaseModel):
-    """ class that represents the user
-    Attributes:
-        email: string - email of the user
-        password: string - user's password
-        first_name: string - user's first name
-        last_name: string -  users's last name
-    """
-
+    """User class that inherits from BaseModel"""
     email = ""
     password = ""
     first_name = ""
     last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """Initializes User instance"""
+        super().__init__(*args, **kwargs)
+
