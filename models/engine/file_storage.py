fe2380e7e9516e9edda692dc407ff1ac93b6dbd4
mosixty1234 <mosixtysipho11@gmail.com>
2023-08-12 16:51:03 -0700

    Revert "task 10"
    This reverts commit f319adee56b31e64f39b89b078d035618a467f0d.

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,7 +3,6 @@
 
 from models.base_model import BaseModel
 import json
-import os
 from models.user import User
 
 
@@ -33,25 +32,6 @@ class FileStorage:
         py_obj = {key: value.to_dict() for key, value in obj_dict.items()}
         with open(FileStorage.__file_path, 'w') as f:
             json.dump(py_obj, f)
-        
-        def classes(self):
-        """This returns a dictionary of valid classes and their references"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.place import Place
-        from models.review import Review
-
-        classes = {"BaseModel": BaseModel,
-                   "User": User,
-                   "State": State,
-                   "City": City,
-                   "Amenity": Amenity,
-                   "Place": Place,
-                   "Review": Review}
-        return classes
 
     def reload(self):
         """Deserialize the JSON file to __objects, if it exists"""
@@ -62,44 +42,5 @@ class FileStorage:
                     class_name = obj["__class__"]
                     del obj["__class__"]
                     self.new(eval(class_name)(**obj))
-
-         def attributes(self):
-        """This returns the valid attributes and their types for classname"""
-        attributes = {
-            "BaseModel":
-                     {"id": str,
-                      "created_at": datetime.datetime,
-                      "updated_at": datetime.datetime},
-            "User":
-                     {"email": str,
-                      "password": str,
-                      "first_name": str,
-                      "last_name": str},
-            "State":
-                     {"name": str},
-            "City":
-                     {"state_id": str,
-                      "name": str},
-            "Amenity":
-                     {"name": str},
-            "Place":
-                     {"city_id": str,
-                      "user_id": str,
-                      "name": str,
-                      "description": str,
-                      "number_rooms": int,
-                      "number_bathrooms": int,
-                      "max_guest": int,
-                      "price_by_night": int,
-                      "latitude": float,
-                      "longitude": float,
-                      "amenity_ids": list},
-            "Review":
-            {"place_id": str,
-                         "user_id": str,
-                         "text": str}
-        }
-        return  attributes
         except FileNotFoundError:
             pass
-
