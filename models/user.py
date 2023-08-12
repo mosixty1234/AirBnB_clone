56af0152e3114e0bc34da71d9d98932fb9962455
mosixty1234 <mosixtysipho11@gmail.com>
2023-08-12 14:24:41 -0700

    user.py

--- models/user.py
+++ models/user.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-"""Defines the User class."""
+"""
+Class User, inherits from BaseModel
+"""
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """Represent a User.
-
-    Attributes:
-        email (str): The email of the user.
-        password (str): The password of the user.
-        first_name (str): The first name of the user.
-        last_name (str): The last name of the user.
     """
-
+    Class attributes
+    """
     email = ""
     password = ""
     first_name = ""
     last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """User init"""
+        super().__init__(*args, **kwargs)
